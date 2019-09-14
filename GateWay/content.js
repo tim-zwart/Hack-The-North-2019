d8c8c96e6d2bb3fbfecec048f9d8924a3f7f8519
Aaron Kong <wqwdkong@gmail.com>
2019-09-14 19:03:46 -0400

    Made it so can shift+hover or hover+shift

--- GateWay/content.js
+++ GateWay/content.js
@@ -1,17 +1,31 @@
 var links = document.getElementsByTagName('a');
 
 
+// Shift then hover to open preview
 for (var i = 0; i < links.length; i++) {
     links[i].addEventListener('mouseover', function (event){
-        if(event.shiftKey){
-            chrome.runtime.sendMessage({
-                event: "link",
-                url: this.href
-            });
-        }
+        this.classList.add("currentlyHovered");        
     });
 }
 
+// Remove hovered class when no longer hovering
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('mouseout', function (event) {
+        this.classList.remove("currentlyHovered");
+    });
+}
+
+// Can also hover then shift
+document.addEventListener ("keydown", function (zEvent) {
+    var items = document.getElementsByClassName("currentlyHovered");
+    if (zEvent.shiftKey && items.length == 1) {
+        alert("Preview");
+        chrome.runtime.sendMessage({
+            event: "link",
+            url: this.href
+        });
+    }
+} );
 
 
 chrome.runtime.onMessage.addListener(
